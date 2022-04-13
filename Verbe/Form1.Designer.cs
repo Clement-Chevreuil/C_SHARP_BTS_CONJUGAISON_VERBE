2595b2587b83685636f916bf454347b9dcb03bca
Clement <clement.chevreuil2000@gmail.com>
2022-04-13 16:42:48 +0200

    Ajoutez des fichiers projet.

---
+++ Verbe/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Verbe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbTemps = new System.Windows.Forms.ComboBox();
+            this.cbPersonne = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTemps = new System.Windows.Forms.Label();
+            this.lblPersonne = new System.Windows.Forms.Label();
+            this.lblVerbe = new System.Windows.Forms.Label();
+            this.btnAleatoire = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.cbVerbe = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbTemps
+            // 
+            this.cbTemps.FormattingEnabled = true;
+            this.cbTemps.Items.AddRange(new object[] {
+            "Présent Indicatif",
+            "Présent Impératif"});
+            this.cbTemps.Location = new System.Drawing.Point(117, 72);
+            this.cbTemps.Name = "cbTemps";
+            this.cbTemps.Size = new System.Drawing.Size(121, 21);
+            this.cbTemps.TabIndex = 1;
+            // 
+            // cbPersonne
+            // 
+            this.cbPersonne.FormattingEnabled = true;
+            this.cbPersonne.Items.AddRange(new object[] {
+            "Je",
+            "Tu",
+            "Il/Elle/On",
+            "Nous",
+            "Vous",
+            "Ils/Elles/Ont"});
+            this.cbPersonne.Location = new System.Drawing.Point(117, 131);
+            this.cbPersonne.Name = "cbPersonne";
+            this.cbPersonne.Size = new System.Drawing.Size(121, 21);
+            this.cbPersonne.TabIndex = 2;
+            this.cbPersonne.SelectedIndexChanged += new System.EventHandler(this.cbPersonne_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // lblTemps
+            // 
+            this.lblTemps.AutoSize = true;
+            this.lblTemps.Location = new System.Drawing.Point(18, 72);
+            this.lblTemps.Name = "lblTemps";
+            this.lblTemps.Size = new System.Drawing.Size(45, 13);
+            this.lblTemps.TabIndex = 4;
+            this.lblTemps.Text = "Temps :";
+            this.lblTemps.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblPersonne
+            // 
+            this.lblPersonne.AutoSize = true;
+            this.lblPersonne.Location = new System.Drawing.Point(18, 134);
+            this.lblPersonne.Name = "lblPersonne";
+            this.lblPersonne.Size = new System.Drawing.Size(58, 13);
+            this.lblPersonne.TabIndex = 5;
+            this.lblPersonne.Text = "Personne :";
+            // 
+            // lblVerbe
+            // 
+            this.lblVerbe.AutoSize = true;
+            this.lblVerbe.Location = new System.Drawing.Point(18, 16);
+            this.lblVerbe.Name = "lblVerbe";
+            this.lblVerbe.Size = new System.Drawing.Size(41, 13);
+            this.lblVerbe.TabIndex = 6;
+            this.lblVerbe.Text = "Verbe :";
+            // 
+            // btnAleatoire
+            // 
+            this.btnAleatoire.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnAleatoire.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAleatoire.Location = new System.Drawing.Point(21, 171);
+            this.btnAleatoire.Name = "btnAleatoire";
+            this.btnAleatoire.Size = new System.Drawing.Size(82, 33);
+            this.btnAleatoire.TabIndex = 7;
+            this.btnAleatoire.Text = "Aléatoire";
+            this.btnAleatoire.UseVisualStyleBackColor = false;
+            this.btnAleatoire.Click += new System.EventHandler(this.btnAleatoire_Click);
+            // 
+            // btnValider
+            // 
+            this.btnValider.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnValider.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnValider.Location = new System.Drawing.Point(163, 171);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(91, 33);
+            this.btnValider.TabIndex = 8;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = false;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // cbVerbe
+            // 
+            this.cbVerbe.FormattingEnabled = true;
+            this.cbVerbe.Items.AddRange(new object[] {
+            "Conjuguer",
+            "Gagner",
+            "Sécher",
+            "Finir",
+            "Choisir",
+            "Frémir",
+            "Prendre",
+            "Peindre",
+            "Rendre"});
+            this.cbVerbe.Location = new System.Drawing.Point(117, 16);
+            this.cbVerbe.Name = "cbVerbe";
+            this.cbVerbe.Size = new System.Drawing.Size(121, 21);
+            this.cbVerbe.TabIndex = 0;
+            this.cbVerbe.SelectedIndexChanged += new System.EventHandler(this.cbVerbe_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(266, 220);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.btnAleatoire);
+            this.Controls.Add(this.lblVerbe);
+            this.Controls.Add(this.lblPersonne);
+            this.Controls.Add(this.lblTemps);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPersonne);
+            this.Controls.Add(this.cbTemps);
+            this.Controls.Add(this.cbVerbe);
+            this.Name = "Form1";
+            this.Text = "Conjugueur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cbTemps;
+        private System.Windows.Forms.ComboBox cbPersonne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTemps;
+        private System.Windows.Forms.Label lblPersonne;
+        private System.Windows.Forms.Label lblVerbe;
+        private System.Windows.Forms.Button btnAleatoire;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.ComboBox cbVerbe;
+    }
+}
+
---
+++ Verbe/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verbe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        List<String> listGroupe = new List<String>();
+        String verbe, personne;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                listGroupe.Add("tenir");
+                listGroupe.Add("venir");
+                listGroupe.Add("acquérir");
+                listGroupe.Add("sentir");
+                listGroupe.Add("vêtir");
+                listGroupe.Add("ouvrir");
+                listGroupe.Add("cueillir");
+                listGroupe.Add("assaillir");
+                listGroupe.Add("faillir");
+                listGroupe.Add("bouillir");
+                listGroupe.Add("dormir");
+                listGroupe.Add("courir");
+                listGroupe.Add("mourir");
+                listGroupe.Add("servir");
+                listGroupe.Add("fuir");
+                listGroupe.Add("gésir");
+                listGroupe.Add("recevoir");
+                listGroupe.Add("voir");
+                listGroupe.Add("aller");
+
+            
+        }
+
+        private void cbVerbe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbPersonne_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAleatoire_Click(object sender, EventArgs e)
+        {
+            Random aleatoire = new Random();
+            int temps = aleatoire.Next(1, 3); 
+            int verbe = aleatoire.Next(1, 10);
+            int personne = aleatoire.Next(1, 7);
+
+            if (temps == 1)
+            {
+                cbTemps.Text = "Présent Indicatif";
+            }
+            else if (temps == 2)
+            {
+                cbTemps.Text = "Présent Impératif";
+            }
+
+            if (verbe == 1)
+            {
+                cbVerbe.Text = "Conjuguer";
+            }
+            else if (verbe == 2)
+            {
+                cbVerbe.Text = "Gagner";
+            }
+            else if (verbe == 3)
+            {
+                cbVerbe.Text = "Sécher";
+            }
+            else if (verbe == 4)
+            {
+                cbVerbe.Text = "Finir";
+            }
+            else if (verbe == 5)
+            {
+                cbVerbe.Text = "Choisir";
+            }
+            else if (verbe == 6)
+            {
+                cbVerbe.Text = "Frémir";
+            }
+            else if (verbe == 7)
+            {
+                cbVerbe.Text = "Prendre";
+            }
+            else if (verbe == 8)
+            {
+                cbVerbe.Text = "Peindre";
+            }
+            else if (verbe == 9)
+            {
+                cbVerbe.Text = "Rendre";
+            }
+             if (personne == 1)
+            {
+                cbPersonne.Text = "Je";
+            }
+            else if (personne == 2)
+            {
+                cbPersonne.Text = "Tu";
+            }
+
+            else if (personne == 3)
+            {
+                cbPersonne.Text = "Il/Elle/On";
+            }
+            else if (personne == 4)
+            {
+                cbPersonne.Text = "Nous";
+            }
+            else if (personne == 5)
+            {
+                cbPersonne.Text = "Vous";
+            }
+            else if (personne == 6)
+            {
+                cbPersonne.Text = "Ils/Elles/Ont";
+            }
+
+
+
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            verbe = cbVerbe.Text;
+            personne = cbPersonne.Text;
+            listGroupe.Contains(verbe);
+
+            if (personne == "Je")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "e");
+
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "s");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "is");
+                }
+            }
+            else if (personne == "Tu")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "es");
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "s");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "is");
+                }
+            }
+            else if (personne == "Il/Elle/On")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "e");
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "t");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "it");
+                }
+            }
+            else if (personne == "Vous")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "ons");
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "ons");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "issons");
+                }
+            }
+            else if (personne == "Nous")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "ez");
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "ez");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "issez");
+                }
+            }
+            else if (personne == "Ils/Elles/Ont")
+            {
+                if (verbe == "etre" || verbe == "avoir")
+                {
+                    MessageBox.Show(verbe + " est un verbe un auxilaire");
+
+                }
+                else if (verbe.EndsWith("er"))
+                {
+                    MessageBox.Show(verbe + "ent");
+                }
+                else if (listGroupe.Contains(verbe) || verbe.EndsWith("oir") || verbe.EndsWith("re"))
+                {
+                    MessageBox.Show(verbe + "ent");
+                }
+                else if (verbe.EndsWith("ir"))
+                {
+                    MessageBox.Show(verbe + "issent");
+                }
+            }
+        }
+    }
+}
